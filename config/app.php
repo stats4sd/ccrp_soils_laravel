b2905e7bd2073856533d07a28f4f518b1deefddc
luciafalcinelli <lucia@stats4sd.org>
2019-06-26 15:59:32 +0100

    translation composer install

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \Tio\Laravel\ServiceProvider::class,
 
     ],
 
