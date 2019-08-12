55fbb3548f3a8ee6d7bef49ee2e0116da263025f
David Mills <d.e.mills@stats4sd.org>
2019-08-12 10:11:40 +0100

    auto-add default locale to path if it doesn't exist

--- routes/web.php
+++ routes/web.php
@@ -11,108 +11,98 @@
 |
 */
 
-//Route::prefix('{locale?}')->middleware('set.locale')->group(function() {
+$routes = function() {
+   Route::get('/', function () {
+     return view('home');
+   });
+
+   Route::get('home', function(){
+      return view('home');
+   });
+
+   Route::get('/admin', function(){
+      return view('home');
+  });
+
+   Route::get('/about', function () {
+       return view('about');
+   });
+
+   Route::get('/start-sampling', function () {
+       return view('start_sampling');
+   });
+
+   Route::get('/qr-codes', function() {
+      return view('qr_code');
+  });
+
+   Route::get('/downloads', function() {
+      return view('downloads');
+  });
+
+   Route::get('/register', function() {
+      return view('register');
+  });
+   Route::get('/downloads', 'DownloadsController@index');
+
+
+
+   Route::get('/{key}/register','RegisterController@index');
+   Route::get('/register', 'RegisterController@index');
+   Route::post('/register/validator', 'RegisterController@validator');
+   Route::post('/register/store', 'RegisterController@store');
+   Route::get('/confirm-project/{project_id}/{user_id}/{key}', 'ConfirmProjectController@index');
+
+
+
+
+
+   Route::group([
+
+       'middleware' => ['auth'],
+
+   ], function () {
+
+   // User profile
+      Route::get('/projects/members/{username}', 'UserAccountController@index');
+      Route::post('/projects/members/{id}/upload', 'UserAccountController@upload');
+      Route::post('/projects/members/{id}/validateDetails', 'UserAccountController@validateDetails');
+      Route::post('/projects/members/{id}/changePassword', 'UserAccountController@changePassword');
+      Route::post('/projects/members/{id}/deleteProfile', 'UserAccountController@deleteProfile');
+      Route::post('/projects/members/{id}/kobo-user', 'UserAccountController@koboUser');
+
+      Route::get('/data-management', function () {
+         return view('data_management');
+     });
+
+
+      Route::get('/create-project', 'CreateProjectController@index');
+      Route::post('/create-project/validateValue', 'CreateProjectController@validateValue');
+
+      Route::post('/create-project/upload', 'CreateProjectController@upload');
+      Route::post('/create-project/store', 'CreateProjectController@store');
+      Route::post('/create-project/send', 'CreateProjectController@sendEmail');
+
+      Route::get('/projects', 'ProjectController@index');
+      Route::get('/projects/{slug}', 'ProjectAccountController@index');
+      Route::post('/projects/{id}/validateGroup', 'ProjectAccountController@validateGroup');
+      Route::post('/projects/{id}/upload', 'ProjectAccountController@upload');
+      Route::post('/projects/{id}/send', 'ProjectAccountController@sendEmail');
+
+      Route::post('/projects/{id}/delete', 'ProjectAccountController@delete');
+      Route::post('/projects/{id}/change-status', 'ProjectAccountController@changeStatus');
+
+
+  });
+};
 
 Route::group([
-  'prefix' => '{locale}',
+  'prefix' => '{locale?}',
   'where' => ['locale' => '[a-zA-Z]{2}'],
-  'middleware' => 'set.locale'], function() {
+  'middleware' => 'set.locale'], $routes);
 
-    Route::get('/', function () {
-        return redirect(app()->getLocale());
-    });
-    Route::get('/home', function(){
-       return view('home');
-   });
-
-    Route::get('/', function(){
-
-       return redirect('en/home');
-   });
-
-    Route::get('/admin', function(){
-       return view('home');
-   });
-
-    Route::get('/about', function () {
-        return view('about');
-    });
-
-    Route::get('/start-sampling', function () {
-        return view('start_sampling');
-    });
-
-    Route::get('/qr-codes', function() {
-       return view('qr_code');
-   });
-
-    Route::get('/downloads', function() {
-       return view('downloads');
-   });
-
-    Route::get('/register', function() {
-       return view('register');
-   });
-    Route::get('/downloads', 'DownloadsController@index');
-
-
-
-    Route::get('/{key}/register','RegisterController@index');
-    Route::get('/register', 'RegisterController@index');
-    Route::post('/register/validator', 'RegisterController@validator');
-    Route::post('/register/store', 'RegisterController@store');
-    Route::get('/confirm-project/{project_id}/{user_id}/{key}', 'ConfirmProjectController@index');
-
-
-
-
-
-    Route::group([
-
-        'middleware' => ['auth'],
-
-    ], function () {
-
-    // User profile
-       Route::get('/projects/members/{username}', 'UserAccountController@index');
-       Route::post('/projects/members/{id}/upload', 'UserAccountController@upload');
-       Route::post('/projects/members/{id}/validateDetails', 'UserAccountController@validateDetails');
-       Route::post('/projects/members/{id}/changePassword', 'UserAccountController@changePassword');
-       Route::post('/projects/members/{id}/deleteProfile', 'UserAccountController@deleteProfile');
-       Route::post('/projects/members/{id}/kobo-user', 'UserAccountController@koboUser');
-
-       Route::get('/data-management', function () {
-          return view('data_management');
-      });
-
-
-       Route::get('/create-project', 'CreateProjectController@index');
-       Route::post('/create-project/validateValue', 'CreateProjectController@validateValue');
-
-       Route::post('/create-project/upload', 'CreateProjectController@upload');
-       Route::post('/create-project/store', 'CreateProjectController@store');
-       Route::post('/create-project/send', 'CreateProjectController@sendEmail');
-
-       Route::get('/projects', 'ProjectController@index');
-       Route::get('/projects/{slug}', 'ProjectAccountController@index');
-       Route::post('/projects/{id}/validateGroup', 'ProjectAccountController@validateGroup');
-       Route::post('/projects/{id}/upload', 'ProjectAccountController@upload');
-       Route::post('/projects/{id}/send', 'ProjectAccountController@sendEmail');
-
-       Route::post('/projects/{id}/delete', 'ProjectAccountController@delete');
-       Route::post('/projects/{id}/change-status', 'ProjectAccountController@changeStatus');
-
-
-   });
-
-
-
-
-
-
-
-});
-
-
+// declare routes without locale prefix;
+Route::group([
+  'middleware' => 'set.locale',], $routes);
 Auth::routes();
 
