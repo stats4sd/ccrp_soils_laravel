87b88ff3e93cb36c01060ed48b3ef6b459a2e6fb
Dave Mills <d.e.mills@stats4sd.org>
2020-06-05 18:09:19 +0100

    adding front-end link for better nav

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,9 +1,11 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class='nav-item'><a class='nav-link' href="{{ route('home') }}"><i class="nav-icon fa fa-home"></i>Back to Front End</a></li>
+
 <li class='nav-item'><a class='nav-link' href="{{ backpack_url('dashboard') }}"><i class="nav-icon fa fa-dashboard"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href="{{ backpack_url('user') }}"><i class='nav-icon fas fa-id-card'></i> Users</a></li>
+<li class='nav-item'><a class='nav-link' href="{{ backpack_url('user') }}"><i class='nav-icon fas fa-user'></i> Users</a></li>
 <li class='nav-item'><a class='nav-link' href="{{ backpack_url('xlsform') }}"><i class='nav-icon fa fa-list-alt'></i> Xlsforms</a></li>
 <li class='nav-item nav-dropdown'>
 <li class='nav-item'><a class='nav-link' href="{{ backpack_url('project') }}"><i class='nav-icon fa fa-file-text-o'></i> Projects</a></li>
-	
+
 
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('datamap') }}'><i class='nav-icon la la-question'></i> DataMaps</a></li>
