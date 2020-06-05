857647fb9a075b2824f84d6ce8dafe9a761cc058
Dave Mills <d.e.mills@stats4sd.org>
2020-06-05 14:58:41 +0100

    fix project image in list

--- resources/views/users/projects.blade.php
+++ resources/views/users/projects.blade.php
@@ -7,7 +7,7 @@
                 @foreach($user->projects as $project)
                     <a href="{{ route('projects.show', $project) }}">
                         <li class="list-group-item img_group_small my-2 align-items-center d-flex flex-row">
-                            <img class="mr-4 img-fluid rounded" src="{{ url($project->avatar) }}" alt="{{ $project->name }} avatar" width="65px"></img>
+                            <img class="mr-4 img-fluid rounded" src="{{ Storage::url($project->avatar) }}" alt="{{ $project->name }} avatar" width="65px"></img>
                             <div class="media-body ml-4 ">
                                 <h5>{{ $project->name }}</h5>
                                 <p>{{ $project->description }}</p>
