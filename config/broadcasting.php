d1644627bd3e76cf7915ed156e6401ef7db725b4
Dave Mills <d.e.mills@stats4sd.org>
2020-06-05 14:58:08 +0100

    pusher-for-server

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,6 +40,20 @@ return [
                 'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => 6001,
+                'scheme' => 'http',
+            ],
+        ],
+
+        'pusher_valet' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001,
                 'scheme' => 'https',
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
