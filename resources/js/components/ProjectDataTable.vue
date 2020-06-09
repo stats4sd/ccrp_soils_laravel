3798525406f2f08ef0ab5b61940cc201006f3fd5
Dave Mills <d.e.mills@stats4sd.org>
2020-06-09 16:30:54 +0100

    WIP

---
+++ resources/js/components/ProjectDataTable.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <div class="container mt-4">
+        Number of soil samples in database: <b> {{ samples.length }}</b>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <table class="table table-striped table-bordered">
+                <tr>
+                    <th>Sample Id</th>
+                    <th>POXC Value</th>
+                    <th>P Value</th>
+                    <th>pH Value</th>
+                    <th>POM Value</th>
+                </tr>
+                <tr v-for="sample in samples" :key="sample.id">
+                    <td>{{ sample.id }}</td>
+                    <td>{{ sample.poxc_result }}</td>
+                    <td>{{ sample.p_result }}</td>
+                    <td>{{ sample.ph_result }}</td>
+                    <td>{{ sample.pom_result }}</td>
+                </tr>
+            </table>
+
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    props: ['project','userId','samples'],
+
+}
+</script>
--- resources/views/projects/tab-data.blade.php
+++ resources/views/projects/tab-data.blade.php
@@ -1,27 +1,2 @@
-<div class="container mt-4">
-    Number of soil samples in database: <b> {{ $project->samples->count() }}</b>
-</div>
-<div class="row">
-    <div class="col-md-6">
-        <table class="table table-striped table-bordered">
-            <tr>
-                <th>Sample Id</th>
-                <th>POXC Value</th>
-                <th>P Value</th>
-                <th>pH Value</th>
-                <th>POM Value</th>
-            </tr>
-            @foreach($project->samples as $sample)
-            <tr>
-                <td>{{ $sample->id }}</td>
-                <td>{{ $sample->poxc_result }}</td>
-                <td>{{ $sample->p_result }}</td>
-                <td>{{ $sample->ph_result }}</td>
-                <td>{{ $sample->pom_result }}</td>
-            </tr>
-            @endforeach
-        </table>
 
-    </div>
-</div>
 <a href="{{ route('projects.downloadsamples', $project) }}" class="btn btn-info">Download Merged Sample Data</a>
