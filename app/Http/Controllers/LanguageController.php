83500023d309be84164052e2fad1cd10ef1892af
Dave Mills <d.e.mills@stats4sd.org>
2020-07-14 10:12:30 +0100

    hotfix for language switching when current route includes parameters

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -12,7 +12,7 @@ class LanguageController extends Controller
         // Take the route to redirect to, and manually replace the lang inside it.
         //// There's bound to be a better / more elegant way of doing this. But at least this is better than having a seperate method for every language!
 
-        $redirect = route($request->redirect);
+        $redirect = $request->redirect;
 
 
         $redirect = str_replace(config('app.url').'/', '', $redirect);
@@ -24,6 +24,7 @@ class LanguageController extends Controller
         $redirect = implode('/', $redirect);
 
         $redirect = $request->lang.'/'.$redirect;
+
 
         return redirect($redirect);
 
