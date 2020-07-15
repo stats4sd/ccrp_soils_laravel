c7bb2202b4aae576202a780f8ce24c48bdfc8fda
Dave Mills <d.e.mills@stats4sd.org>
2020-07-15 15:04:21 +0100

    adding validation to check for active kobo username

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Models\User;
 use App\Models\Invite;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 
 class UserObserver
 {
@@ -27,11 +28,40 @@ class UserObserver
             $invite->confirm();
         }
 
+
+
     }
 
     public function saving (User $user)
     {
        $user->slug = Str::slug($user->email);
+
+    //    //If Kobousername is updated, check it's real.
+    //     if($user->isDirty("kobo_id")) {
+
+    //         $testForm = config('services.kobo.test_form');
+    //         $endPoint = config('services.kobo.endpoint_v2');
+
+    //         $payload = [
+    //             'permission' => $endPoint . '/permissions/add_submissions/',
+    //             'user' => $endPoint . '/users/' . $this->username . '/',
+    //         ];
+
+    //         $response = Http::withBasicAuth(config('services.kobo.username'), config('services.kobo.password'))
+    //             ->withHeaders(['Accept' => 'application/json'])
+    //             ->post($endPoint . '/assets/' . $testForm . '/permission-assignments/', $payload)
+    //             ->json();
+
+    //         if($response->successful()) {
+    //             return true;
+    //         }
+
+    //         if($response->failed()) {
+    //             return false;
+    //         }
+
+        //}
+
     }
 
 }
